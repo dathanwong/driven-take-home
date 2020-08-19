a4a5ab2020251594adc9f49cf607c184159dde2b
dathwo@outlook.com <dathwo@outlook.com>
2020-08-19 14:47:18 -0700

    Added styling for clicked piece

--- src/Components/CheckerPiece.js
+++ src/Components/CheckerPiece.js
@@ -3,10 +3,10 @@ import './CheckerPieceStyle.css';
 
 const CheckerPiece = (props) => {
 
-    const {color, left, shape} = props;
+    const {color, left, shape, isClicked} = props;
 
     return ( 
-        <div className={"checker-piece" + " " + "checker-piece-"+shape} style={{backgroundColor:color, left:left+"px"}}></div>
+        <div className={"checker-piece" + " " + "checker-piece-"+shape + " " + (isClicked===true ? "checker-piece-clicked" : "")} style={{backgroundColor:color, left:left+"px"}}></div>
      );
 }
  
